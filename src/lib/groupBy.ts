cb1c25fcf8cd73d419144904c6aef926b8308ea0
Rishabh Parsediya <parsediyarishabh@gmail.com>
2024-05-16 20:46:07 +0530

    group by bug

--- src/lib/groupBy.ts
+++ src/lib/groupBy.ts
@@ -5,11 +5,23 @@ interface Ticket {
   ticketId: string;
 }
 export function groupByTag(data: Ticket[]) {
-  const groupedByTag2 = Object.groupBy(
-    data,
-    (item: Ticket) => item.tag
-  ) as Record<string, Ticket[]>;
-  return groupedByTag2;
+  interface Item {
+    heading: string;
+    content: string;
+    tag: string;
+    ticketId: string;
+  }
+  const groupedByTag: Record<string, Item[]> = data.reduce(
+    (acc: Record<string, Item[]>, item: Item) => {
+      const tag = item.tag;
+      acc[tag] = acc[tag] || [];
+      acc[tag].push(item);
+      return acc;
+    },
+    {}
+  );
+
+  return groupedByTag
 }
 export function findById(data: Ticket[], ticketId: string | null) {
   return data.filter((tick) => tick.ticketId === ticketId);
