f86ca99e082ea93f74979dc261e4b16d5eec6d0a
Rishabh Parsediya <parsediyarishabh@gmail.com>
2024-05-16 22:03:22 +0530

    refactor connection db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,25 +1,24 @@
-import { MongoClient, MongoClientOptions } from "mongodb";
-
-declare const global: {
-  _mongoClientPromise?: Promise<MongoClient>;
-};
-
-
-const uri: string = process.env.MONGO_URI || "";
-
-let client: MongoClient | undefined;
-let clientPromise: Promise<MongoClient>;
+import { MongoClient } from "mongodb";
 
 if (!process.env.MONGO_URI) {
-  throw new Error("Add Mongo URI to .env.local");
+  throw new Error("Please add your Mongo URI to .env.local");
 }
 
+const uri: string = process.env.MONGO_URI;
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
 if (process.env.NODE_ENV === "development") {
-  if (!global._mongoClientPromise) {
+
+  let globalWithMongoClientPromise = global as typeof globalThis & {
+    _mongoClientPromise: Promise<MongoClient>;
+  };
+
+  if (!globalWithMongoClientPromise._mongoClientPromise) {
     client = new MongoClient(uri);
-    global._mongoClientPromise = client.connect();
+    globalWithMongoClientPromise._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = globalWithMongoClientPromise._mongoClientPromise;
 } else {
   client = new MongoClient(uri);
   clientPromise = client.connect();
